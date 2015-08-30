51c754d6169134ac9e0928e9312e43cf2140a406
Lucas Message <lucas.message@hotmail.com>
2015-08-30 16:00:03 -0500

    Missing file added.

---
+++ Adam/Adam/Characters/PlayerProperties.cs
@@ -0,0 +1,19 @@
+﻿using Adam.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Adam
+{
+    public partial class Player : Entity
+    {
+        Timer onFireTimer = new Timer();
+        Timer fireTickTimer = new Timer();
+
+        /// <summary>
+        /// Returns true if player is on fire and is taking damage per second.
+        /// </summary>
+        public bool IsOnFire { get; set; }
+    }
+}
