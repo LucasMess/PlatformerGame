c6a1c70d3a1d608a454b56d897296e21f2fa976f
DESKTOP-PHAPI9Q\Lucas <lucas.message@yahoo.com>
2016-12-21 18:59:21 -0600

    Basics of level grid done. Need to work on level format first.

---
+++ Adam/UI/MainMenu/LevelGrid.cs
@@ -0,0 +1,31 @@
+﻿using Adam.UI.Elements;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace Adam.UI.MainMenu
+{
+    /// <summary>
+    /// Used to show the levels in squares numbered 0-n in a grid layout.
+    /// </summary>
+    class LevelGrid
+    {
+        class LevelSquare : TextButton
+        {
+            public LevelSquare(Vector2 position, string text, bool convertCoordinates = true) : base(position, text, convertCoordinates)
+            {
+
+            }
+        }
+
+        List<LevelSquare> levelSquares = new List<LevelSquare>();
+
+        /// <summary>
+        /// Creates a new level grid from the files in a folder with the given name.
+        /// </summary>
+        /// <param name="levelTheme"></param>
+        public LevelGrid(string levelTheme)
+        {
+
+        }
+    }
+}
---
+++ Adam/UI/MainMenu/StoryMode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Adam.UI.MainMenu
+{
+    class StoryMode
+    {
+        List<LevelGrid> levelSets = new List<LevelGrid>();
+
+        string[] levelOrder = new string[]
+        {
+            "grasslands", "desert",
+        };
+
+        int currentLevelSet = 0;
+
+        public StoryMode()
+        {
+            levelSets.Add(new LevelGrid(levelOrder[currentLevelSet]));
+        }
+    }
+}
