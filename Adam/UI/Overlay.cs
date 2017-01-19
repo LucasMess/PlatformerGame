f27a0c048d36d4f20c2887daef0e9d50be251ddb
DESKTOP-PHAPI9Q\Lucas <lucas.message@yahoo.com>
2017-01-19 14:18:48 -0600

    Simplified overlay so that there are less small variables.

--- Adam/UI/Overlay.cs
+++ Adam/UI/Overlay.cs
@@ -10,47 +10,151 @@ namespace Adam.UI
     /// </summary>
     public static class Overlay
     {
-        private const float OpacityDelta = .01f;
-        private static float _whiteOpacity;
-        private static bool _isFlashingWhite;
-        private static bool _hasReachedMaxWhite;
-        private static Texture2D _white = ContentHelper.LoadTexture("Tiles/white");
-
-        public static void Update()
+        /// <summary>
+        /// Used to briefly do a camera flash on the screen.
+        /// </summary>
+        private static class WhiteFlash
         {
-            if (_isFlashingWhite)
+            private const float OpacityDelta = .01f;
+            private static float _whiteOpacity;
+            private static bool _isFlashingWhite;
+            private static bool _hasReachedMaxWhite;
+            private static Texture2D _white = ContentHelper.LoadTexture("Tiles/white");
+
+            public static void Update()
             {
-                if (!_hasReachedMaxWhite)
+                if (_isFlashingWhite)
                 {
-                    _whiteOpacity += OpacityDelta * 5;
-                    if (_whiteOpacity > 1f)
+                    if (!_hasReachedMaxWhite)
                     {
-                        _hasReachedMaxWhite = true;
+                        _whiteOpacity += OpacityDelta * 5;
+                        if (_whiteOpacity > 1f)
+                        {
+                            _hasReachedMaxWhite = true;
+                        }
+                    }
+                    else
+                    {
+                        _whiteOpacity -= OpacityDelta;
+                        if (_whiteOpacity < 0)
+                        {
+                            _whiteOpacity = 0;
+                            _hasReachedMaxWhite = false;
+                            _isFlashingWhite = false;
+                        }
+                    }
+                }
+            }
+
+            public static void Start()
+            {
+                _isFlashingWhite = true;
+                _hasReachedMaxWhite = false;
+                _whiteOpacity = 1f;
+            }
+
+            public static void Draw(SpriteBatch spriteBatch)
+            {
+                spriteBatch.Draw(_white, new Rectangle(0, 0, AdamGame.UserResWidth, AdamGame.UserResHeight), Color.White * _whiteOpacity);
+            }
+        }
+        /// <summary>
+        /// Used to fade in and out to black.
+        /// </summary>
+        private static class BlackFade
+        {
+            private static bool _isActive = false;
+            private static bool _isFadingIn = false;
+            private static float _opacity = 0f;
+            private const float MaxOpacity = 1f;
+            private const float MinOpacity = 0f;
+
+            private static float GetOpacityDelta(int speed)
+            {
+                switch (speed)
+                {
+                    case 1:
+                        return .05f;
+
+                    case 2:
+                        return .06f;
+
+                    case 3:
+                        return .07f;
+
+                    case 4:
+                        return .08f;
+
+                    case 5:
+                        return .09f;
+                }
+
+                return .05f;
+            }
+            private static int _speed;
+
+            public static void Update()
+            {
+                if (_isFadingIn)
+                {
+                    _opacity -= GetOpacityDelta(_speed);
+
+                    if (_opacity < MinOpacity)
+                    {
+                        _opacity = MinOpacity;
                     }
                 }
                 else
                 {
-                    _whiteOpacity -= OpacityDelta;
-                    if (_whiteOpacity < 0)
+                    _opacity += GetOpacityDelta(_speed);
+
+                    if (_opacity > MaxOpacity)
                     {
-                        _whiteOpacity = 0;
-                        _hasReachedMaxWhite = false;
-                        _isFlashingWhite = false;
+                        _opacity = MaxOpacity;
                     }
                 }
             }
+
+            /// <summary>
+            /// Fades in using the given speed.
+            /// </summary>
+            /// <param name="speed">1 = slowest, 5 = fastest.</param>
+            public static void FadeIn(int speed = 1)
+            {
+                _speed = speed;
+                _isFadingIn = true;
+            }
+
+            /// <summary>
+            /// Fades in using the given speed.
+            /// </summary>
+            /// <param name="speed">1 = slowest, 5 = fastest.</param>
+            public static void FadeOut(int speed = 1)
+            {
+                _speed = speed;
+                _isFadingIn = false;
+            }
+        }
+
+
+
+
+        // Variables for fade in and out.
+
+
+        public static void Update()
+        {
+            WhiteFlash.Update();
         }
 
         public static void FlashWhite()
         {
-            _isFlashingWhite = true;
-            _hasReachedMaxWhite = false;
-            _whiteOpacity =1f;
+            WhiteFlash.Start();
         }
 
         public static void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(_white, new Rectangle(0,0, AdamGame.UserResWidth, AdamGame.UserResHeight), Color.White * _whiteOpacity);
+            WhiteFlash.Draw(spriteBatch);
         }
     }
 }
